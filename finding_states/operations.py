5dc01301f16faf75094ae6097213e534e8923702
brayjmendoza <brayjmendoza@gmail.com>
2025-02-24 20:21:12 -0800

    moved rotation tests into operations and did optimization for RiccardiWitness

--- finding_states/operations.py
+++ finding_states/operations.py
@@ -54,3 +54,36 @@ def rotate_m(m, n):
     return n @ m @ adjoint(n)
 
 
+if __name__ == "__main__":
+
+    print("===== PAULI_X about z =====")
+    print("Actual: \n", op.rotate_z(PAULI_X, theta), "\n")
+    print("Predicted: \n", np.cos(theta)*PAULI_X + np.sin(theta)*PAULI_Y, "\n")
+
+    print("===== PAULI_Y about z =====")
+    print("Actual: \n", op.rotate_z(PAULI_Y, theta), "\n")
+    print("Predicted: \n", np.cos(theta)*PAULI_Y - np.sin(theta)*PAULI_X, "\n")
+
+    print("===== PAULI_X about y =====")
+    print("Actual: \n", op.rotate_y(PAULI_X, theta), "\n")
+    print("Predicted: \n", np.cos(theta)*PAULI_X - np.sin(theta)*PAULI_Z, "\n")
+
+    print("===== PAULI_Z about y =====")
+    print("Actual: \n", op.rotate_y(PAULI_Z, theta), "\n")
+    print("Predicted: \n", np.cos(theta)*PAULI_Z + np.sin(theta)*PAULI_X, "\n")
+
+    print("===== PAULI_Y about x =====")
+    print("Actual: \n", op.rotate_x(PAULI_Y, theta), "\n")
+    print("Predicted: \n", np.cos(theta)*PAULI_Y - np.sin(theta)*PAULI_Z, "\n")
+
+    print("===== PAULI_Z about x =====")
+    print("Actual: \n", op.rotate_x(PAULI_Z, theta), "\n")
+    print("Predicted: \n", np.cos(theta)*PAULI_Z + np.sin(theta)*PAULI_Y, "\n")
+
+    ### Predicted - Actual (expected: all 0s for all ###
+    # print((np.cos(theta)*PAULI_X + np.sin(theta)*PAULI_Y) - rotate_z(PAULI_X, theta))
+    # print((np.cos(theta)*PAULI_Y - np.sin(theta)*PAULI_X) - rotate_z(PAULI_Y, theta))
+    # print((np.cos(theta)*PAULI_X - np.sin(theta)*PAULI_Z) - rotate_y(PAULI_X, theta))  
+    # print((np.cos(theta)*PAULI_Z + np.sin(theta)*PAULI_X) - rotate_y(PAULI_Z, theta))
+    # print((np.cos(theta)*PAULI_Y - np.sin(theta)*PAULI_Z) - rotate_x(PAULI_Y, theta))
+    # print((np.cos(theta)*PAULI_Z + np.sin(theta)*PAULI_Y) - rotate_x(PAULI_Z, theta))
